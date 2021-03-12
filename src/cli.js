78b670b46f4f591e634aff514b1272b1d4e28a2b
Eudgene <55323268+Eudgene@users.noreply.github.com>
2021-03-12 08:41:31 +0300

    Update

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 export const userName = () => {
-  var name = readlineSync.question("May I have your name? ");
+  var name = readlineSync.question('May I have your name? ');
   return name;
 };
