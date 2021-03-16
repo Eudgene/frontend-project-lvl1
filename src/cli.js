5037c292a0401be225e5126bb24b2c4297d8c546
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-16 08:15:53 +0300

    deletd semicolons

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 const userName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  return name;
-};
-export default userName;
+  const name = readlineSync.question('May I have your name? ')
+  return name
+}
+export default userName
