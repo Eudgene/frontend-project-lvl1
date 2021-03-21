c573292cc9463c420e04c0ed7f22622b710e9374
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-21 12:15:50 +0300

    test3 back to concatanation

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,5 +3,5 @@
 import userName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-//console.log(`Hi ${userName}!`);
-console.log(`${userName}`);
+console.log('Hi' + userName + '!');
+
