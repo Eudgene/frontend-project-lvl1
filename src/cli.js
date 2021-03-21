b628db301e9b5c97dc1fe1f524afc51fa7114350
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-21 12:33:42 +0300

    test -changed cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => {
+/*const userName = () => {
   const name = readlineSync.question('May I have your name? ');
   return name;
-};
-export default userName;
+};*/
+export const name = readlineSync.question('May I have your name? ');
