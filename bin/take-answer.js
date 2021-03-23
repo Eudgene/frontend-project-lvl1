520b40eea3c072bb87df182a2c7949fff5ea3bb1
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-23 08:25:56 +0300

    add var to index

--- bin/take-answer.js
+++ bin/take-answer.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 
 const answers = ['Yes', 'No'],
-const index = readlineSync.keyInSelect(answers, 'Answer "yes" if the number is even, otherwise answer "no". Question: 15');
+var index = readlineSync.keyInSelect(answers, 'Answer "yes" if the number is even, otherwise answer "no". Question: 15');
 export {answers, index};
