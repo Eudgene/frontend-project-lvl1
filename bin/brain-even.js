878b38d040d0d0fce4a53225c421d941499d30a3
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-23 08:13:33 +0300

    change import in brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import name from 'take-name-of-user.js';
+import name from '../bin/take-name-of-user.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hi, ${name}!`);
