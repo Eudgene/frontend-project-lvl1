3d7884cf268eb439060293933317788296145ffb
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-23 08:22:37 +0300

    added changes to brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import name from '../bin/take-name-of-user.js';
+import {answers, index} from '../bin/take-answer.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hi, ${name}!`);
+console.log(`${answers[index]}`);
--- bin/take_answer.js
+++
@@ -1,5 +0,0 @@
-import readlineSync from 'readline-sync';
-
-answers = ['Yes', 'No'],
-index = readlineSync.keyInSelect(answers, 'Answer "yes" if the number is even, otherwise answer "no". Question: 15');
-export {answers, index};
