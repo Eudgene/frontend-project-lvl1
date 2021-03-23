47535fcd8ea0155f438b4f83b7b4bc9e2d333d7f
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-23 08:33:18 +0300

    fixed after linter check

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import name from '../bin/take-name-of-user.js';
-import {answers, index} from '../bin/take-answer.js';
+import name from './take-name-of-user.js';
+import { answers, index } from './take-answer.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hi, ${name}!`);
