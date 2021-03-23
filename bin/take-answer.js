731b6c51e5d4119d8bd3ad15ad6243eb5e299325
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-23 08:40:21 +0300

    test commit

--- bin/take-answer.js
+++ bin/take-answer.js
@@ -2,4 +2,4 @@ import readlineSync from 'readline-sync';
 
 const answers = ['Yes', 'No'];
 const index = readlineSync.keyInSelect(answers, 'Answer "yes" if the number is even, otherwise answer "no". Question: 15');
-export { answers, index };
+export { answers, index }
