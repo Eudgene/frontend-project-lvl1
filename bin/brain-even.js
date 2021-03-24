b382fde34ee3b67dd9fe779dc3e93f14aad9f047
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-24 08:01:59 +0300

    test2 with console.log

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 import name from './take-name-of-user.js';
-import { answers, index } from './take-answer.js';
+
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hi, ${name}!`);
-//console.log(`${answers[index]}`);
+import { answers, index } from './take-answer.js';
+console.log(`${answers[index]}`);
