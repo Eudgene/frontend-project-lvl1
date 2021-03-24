acbbf51d644079e5278b66ee873318a8e48ee6af
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-24 08:23:15 +0300

    add guestion

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-//import { name } from './take-name-of-user.js';
-//import { answers, index } from './take-answer.js';
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
@@ -9,6 +7,6 @@ console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hi, ${name}!`);
 
-const answers = ['Yes', 'No'];
-const index = readlineSync.keyInSelect(answers, 'Answer "yes" if the number is even, otherwise answer "no". Question: 15');
-//console.log(`${answers[index]}`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const answer = readlineSync.question(`Question: ${}`);
+
