9d21c70a439112dbb814a797772dfd0e88c37113
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-24 08:00:52 +0300

    test

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,4 +5,4 @@ import { answers, index } from './take-answer.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hi, ${name}!`);
-console.log(`${answers[index]}`);
+//console.log(`${answers[index]}`);
