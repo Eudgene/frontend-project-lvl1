72ac9c9ea63b5a4b2156fdb130b6f3658cf9f39f
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-24 08:17:15 +0300

    test9

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-import { name } from './take-name-of-user.js';
+//import { name } from './take-name-of-user.js';
 //import { answers, index } from './take-answer.js';
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
+
+const name = readlineSync.question('May I have your name? ');
 console.log(`Hi, ${name}!`);
 //console.log(`${answers[index]}`);
