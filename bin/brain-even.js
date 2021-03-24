f168dc60b2fc905a00292237faad03bca188f39a
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-24 08:26:41 +0300

    add guestion

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -8,5 +8,5 @@ const name = readlineSync.question('May I have your name? ');
 console.log(`Hi, ${name}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const answer = readlineSync.question(`Question: \n Your answer: `);
+const answer = readlineSync.question(`Question: \nYour answer: `);
 
