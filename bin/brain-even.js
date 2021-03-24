379271e08ff45728e7c44a16357ba54f4e30726b
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-24 08:11:28 +0300

    test5 with console.log

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-
+console.log('Welcome to the Brain Games!');
 import name from './take-name-of-user.js';
 //import { answers, index } from './take-answer.js';
 
-console.log('Welcome to the Brain Games!');
+
 console.log(`Hi, ${name}!`);
 //console.log(`${answers[index]}`);
