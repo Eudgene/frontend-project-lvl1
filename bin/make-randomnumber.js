e6a19ca0dafb191b1399dba7aed3983f1ed3dd47
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 07:41:45 +0300

    add if form

--- bin/make-randomnumber.js
+++ bin/make-randomnumber.js
@@ -1,4 +1,14 @@
+import { randomnumber } from './brain-even.js';
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+};
+function isNumberIsEven(randomnumber) {
+  if(randomnumber%2 === 0) {
+    return 'yes';
+  }
+  return 'no';
 }
+console.log(isNumberIsEven());
+
 export default getRandomInt;
