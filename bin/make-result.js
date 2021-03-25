35f6423f2bdcdf5b0d50da6b9511962801d2fd41
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 08:52:01 +0300

    fix import in make-result

--- bin/make-result.js
+++ bin/make-result.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 import getRandomInt from './make-randomnumber.js';
-import {isNumberIsEven} from './make-randomnumber.js';
+import { isNumberIsEven } from './make-randomnumber.js';
 import name from './brain-even.js';
 
 let counter = 0;
