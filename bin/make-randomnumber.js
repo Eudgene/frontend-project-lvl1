c49571a6d78f236285f9901e376ae4dbec2342eb
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 08:18:03 +0300

    fix make-randomnumber

--- bin/make-randomnumber.js
+++ bin/make-randomnumber.js
@@ -1,11 +1,9 @@
-import { randomnumber } from './brain-even.js';
-
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-export function isNumberIsEven(randomnumber) {
-  if(randomnumber % 2 === 0) {
+export function isNumberIsEven(number) {
+  if(number % 2 === 0) {
     return 'yes';
   } else {
     return 'no';
