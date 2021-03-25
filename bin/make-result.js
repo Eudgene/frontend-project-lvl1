eca33f8649124c25b035cce619864e7532b5c7eb
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 08:59:28 +0300

    fix after linter

--- bin/make-result.js
+++ bin/make-result.js
@@ -1,26 +1,25 @@
 import readlineSync from 'readline-sync';
-import getRandomInt from './make-randomnumber.js';
-import { isNumberIsEven } from './make-randomnumber.js';
+import { isNumberIsEven, getRandomInt } from './make-randomnumber.js';
 import name from './brain-even.js';
 
 let counter = 0;
-export function makeResult() {
-    const randomnumber = getRandomInt(100);
-    const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
+function makeResult() {
+  const randomnumber = getRandomInt(100);
+  const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
     
-    if(answer === isNumberIsEven(randomnumber) && counter < 2) {
-      console.log('Correct!');
-      counter += 1;
-      makeResult();
-    } else if(answer === isNumberIsEven(randomnumber) && counter === 2){
-      console.log(`Correct! \nCongratulations, ${name}!`); 
+  if(answer === isNumberIsEven(randomnumber) && counter < 2) {
+    console.log('Correct!');
+    counter += 1;
+    makeResult();
+  } else if(answer === isNumberIsEven(randomnumber) && counter === 2){
+    console.log(`Correct! \nCongratulations, ${name}!`); 
+  } else {
+    if (answer === 'yes' || answer === 'no') {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was ${answer === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`);
     } else {
-      if (answer === 'yes' || answer === 'no') {
-        console.log(`"${answer}" is wrong answer ;(. Correct answer was ${answer === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`);
-      } else {
-        console.log(`"${answer}" is wrong answer ;(. Correct answer was ${isNumberIsEven(randomnumber) === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`); 
-      }
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was ${isNumberIsEven(randomnumber) === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`); 
     }
+  }
 }
 
-
+export default makeResult;
