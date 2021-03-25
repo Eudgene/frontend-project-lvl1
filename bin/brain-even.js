ad78244342cdb38c1eda4246f95cd6d841c4e362
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 08:30:13 +0300

    fix brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import getRandomInt from './make-randomnumber.js';
-import {isNumberIsEven} from './make-randomnumber.js';
+//import getRandomInt from './make-randomnumber.js';
+//import {isNumberIsEven} from './make-randomnumber.js';
+import {makeResult} from './make-result.js';
 
 console.log('Welcome to the Brain Games!');
 
@@ -10,14 +11,15 @@ const name = readlineSync.question('May I have your name? ');
 console.log(`Hi, ${name}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const randomnumber = getRandomInt(100);
+makeResult();
+/*const randomnumber = getRandomInt(100);
 const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
 
-if(answer === isNumberIsEven(randomnumber)) {
-  console.log('Correct!')
+let counter = 0;
+if(answer === isNumberIsEven(randomnumber) && counter !== 3) {
+  console.log('Correct!');
+  counter += 1;
   const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
 } else {
   console.log(`"${answer}" is wrong answer ;(. Correct answer was ${answer === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`);
-}
-
-export {randomnumber, answer};
+}*/
