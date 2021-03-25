dc25fa00ca5fc013b17becd7e98dcac7dc380a8c
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 17:58:29 +0300

    fix after linter

--- bin/make-result.js
+++ bin/make-result.js
@@ -6,19 +6,17 @@ let counter = 0;
 function makeResult() {
   const randomnumber = getRandomInt(100);
   const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
-    
   if (answer === isNumberIsEven(randomnumber) && counter < 2) {
     console.log('Correct!');
     counter += 1;
     makeResult();
-  } else if (answer === isNumberIsEven(randomnumber) && counter === 2){
-    console.log(`Correct! \nCongratulations, ${name}!`); 
+  } else if (answer === isNumberIsEven(randomnumber) && counter === 2) {
+    console.log(`Correct! \nCongratulations, ${name}!`);
   } else {
     if (answer === 'yes' || answer === 'no') {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was ${answer === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`);
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was ${isNumberIsEven(randomnumber) === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`); 
     }
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was ${isNumberIsEven(randomnumber) === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`); 
   }
 }
 
