41f84f35ade173b0c397a9cca6fd2ca1ae3a6835
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 08:38:04 +0300

    fix makeResult5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,7 +7,7 @@ import {makeResult} from './make-result.js';
 
 console.log('Welcome to the Brain Games!');
 
-const name = readlineSync.question('May I have your name? ');
+export const name = readlineSync.question('May I have your name? ');
 console.log(`Hi, ${name}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
