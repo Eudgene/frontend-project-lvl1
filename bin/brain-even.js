a63cb8d5645d354737e1de0f3ff065b7c7cf461f
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-25 08:12:19 +0300

    test conditional statement if

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -12,13 +12,12 @@ console.log(`Hi, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const randomnumber = getRandomInt(100);
 const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
-console.log(answer);
-console.log(isNumberIsEven(randomnumber));
+
 if(answer === isNumberIsEven(randomnumber)) {
   console.log('Correct!')
   //const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
 } else {
-  console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+  console.log(`${answer} is wrong answer ;(. Correct answer was ${answer === 'yes' ? 'no' : 'yes'}.\nLet's try again, ${name}!`);
 }
 
 export {randomnumber, answer};
