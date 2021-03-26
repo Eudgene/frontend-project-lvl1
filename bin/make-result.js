21f0b52d0f0e631da9103b9929b392c514c393c4
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-26 08:00:54 +0300

    fix after linter

--- bin/make-result.js
+++ bin/make-result.js
@@ -13,11 +13,7 @@ function makeResult() {
   } else if (answer === isNumberIsEven(randomnumber) && counter === 2) {
     console.log(`Correct! \nCongratulations, ${name}!`);
   } else {
-    if (answer === 'yes' || answer === 'no') {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was ${answer === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`);
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was ${isNumberIsEven(randomnumber) === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`);
-    }
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was ${isNumberIsEven(randomnumber) === 'yes' ? '"no"' : '"yes"'}.\nLet's try again, ${name}!`);
   }
 }
 
