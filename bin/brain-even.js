b070d983f1cd650296498fc33b8dcb9fd07a68fb
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-26 08:30:20 +0300

    test with one file

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import { getRandomInt, isNumberIsEven } from './make-randomnumber.js';
 import readlineSync from 'readline-sync';
 import makeResult from './make-result.js';
 
@@ -9,6 +9,21 @@ const name = readlineSync.question('May I have your name? ');
 console.log(`Hi, ${name}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-makeResult();
+//makeResult();
 
-export default name;
+//export default name;
+let counter = 0;
+function makeResult() {
+  const randomnumber = getRandomInt(100);
+  const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
+  if (answer === isNumberIsEven(randomnumber) && counter < 2) {
+    console.log('Correct!');
+    counter += 1;
+    makeResult();
+  } else if (answer === isNumberIsEven(randomnumber) && counter === 2) {
+    console.log(`Correct! \nCongratulations, ${name}!`);
+  } else {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${isNumberIsEven(randomnumber)}".\nLet's try again, ${name}!`);
+  }
+}
+makeResult();
