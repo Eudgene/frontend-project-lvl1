bab41dffb88d09fe3ce415363cb314e86ee18a69
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-28 10:42:17 +0300

    test2 with three files

--- bin/make-result.js
+++ bin/make-result.js
@@ -1,11 +1,11 @@
 
 
 function makeResult(number, ans, count) {
-    if (ans === isNumberIsEven(number) && count < 2) {
+    if (ans === number && count < 2) {
       console.log('Correct!');
       counter += 1;
       makeResult();
-    } else if (ans === isNumberIsEven(number) && count === 2) {
+    } else if (ans === number && count === 2) {
       console.log(`Correct! \nCongratulations, ${name}!`);
     } else {
       console.log(`"${ans}" is wrong answer ;(. Correct answer was "${isNumberIsEven(number)}".\nLet's try again, ${name}!`);
