fe8674557ffa93dbfb1873de71963abc7418a8d1
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-29 08:39:25 +0300

    add get-name.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 import makeResult from './make-result.js';
-import * as name from './get-name.js';
+import nameOfUser from './get-name.js';
 
 console.log('Welcome to the Brain Games!');
-console.log(name.nameOfUser());
+console.log(nameOfUser());
 console.log(`Hi, ${name.nameOfUser}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
