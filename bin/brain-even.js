49aea7b52fbd6f188afae3e13aefdfbf97a7c25d
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-29 08:10:41 +0300

    change name to function

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,8 +5,10 @@ import makeResult from './make-result.js';
 
 console.log('Welcome to the Brain Games!');
 
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hi, ${name}!`);
+function name() {
+  readlineSync.question('May I have your name? ');
+}
+console.log(`Hi, ${name()}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
