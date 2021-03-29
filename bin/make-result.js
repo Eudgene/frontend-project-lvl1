9aaaaac1a99fa1b78de3dc410e3e4ad9ce4bd619
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-29 08:43:41 +0300

    add get-name.js

--- bin/make-result.js
+++ bin/make-result.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 import nameOfUser from './get-name.js';
 import { getRandomInt, isNumberIsEven } from './make-randomnumber.js';
 
+const name = nameOfUser();
 function makeResult() {
   const randomnumber = getRandomInt(100);
   const ynanswer = isNumberIsEven(randomnumber);
@@ -9,7 +10,7 @@ function makeResult() {
   if (answer === ynanswer) {
     console.log('Correct!');
   } else {
-    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".\nLet's try again, ${nameOfUser()}!`);
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".\nLet's try again, ${name}!`);
   }
 }
 
