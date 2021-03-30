bbe741eb291f00bf8f0201e27f63f0d2bbd9c982
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-30 08:12:03 +0300

    fix make-result

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 import makeResult from './make-result.js';
-//import getnameOfUser from './get-name.js';
+import getnameOfUser from './get-name.js';
 
 console.log('Welcome to the Brain Games!');
-//const nameOfUser = getnameOfUser();
+const nameOfUser = getnameOfUser();
 console.log(`Hi, ${nameOfUser}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
