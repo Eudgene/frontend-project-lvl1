de31503929bb9afcfc0887f466d117ab0e03bdff
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-30 08:13:36 +0300

    fix make-result

--- bin/make-result.js
+++ bin/make-result.js
@@ -2,7 +2,6 @@ import readlineSync from 'readline-sync';
 //import getnameOfUser from './get-name.js';
 import { getRandomInt, isNumberIsEven } from './make-randomnumber.js';
 
-//const nameOfUser = getnameOfUser();
 let counter = 0;
 function makeResult() {
   const randomnumber = getRandomInt(100);
@@ -17,6 +16,7 @@ function makeResult() {
       console.log(`Congratulations, ${nameOfUser}!`);
     }
   } else {
+    const nameOfUser = getnameOfUser();
     console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".\nLet's try again, ${nameOfUser}!`);
   }
 }
