3d5f11e2cacafcd89915ea899204d80f8e47d3e0
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-30 07:53:03 +0300

    fix get-name

--- bin/make-result.js
+++ bin/make-result.js
@@ -11,7 +11,6 @@ function makeResult() {
     console.log('Correct!');
   } else {
     console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".\nLet's try again, ${nameOfUser}!`);
-    break;
   }
 }
 
