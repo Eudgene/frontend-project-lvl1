5a762c9f780159d9924b9c8a6689a411a0bffae5
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-30 07:59:05 +0300

    fix make-result

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -9,10 +9,4 @@ console.log(`Hi, ${nameOfUser}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-let counter = 0;
-while (counter < 2) {
-  counter += 1;
-  makeResult();
-}
 makeResult();
-console.log(`Congratulations, ${nameOfUser}!`);
--- bin/make-result.js
+++ bin/make-result.js
@@ -3,12 +3,19 @@ import getnameOfUser from './get-name.js';
 import { getRandomInt, isNumberIsEven } from './make-randomnumber.js';
 
 const nameOfUser = getnameOfUser();
+let counter = 0;
 function makeResult() {
   const randomnumber = getRandomInt(100);
   const ynanswer = isNumberIsEven(randomnumber);
   const answer = readlineSync.question(`Question: ${randomnumber}\nYour answer: `);
   if (answer === ynanswer) {
-    console.log('Correct!');
+    if (counter < 2) {
+      console.log('Correct!');
+      counter += 1;
+      makeResult();
+    } else if (counter === 2) {
+      console.log(`Congratulations, ${nameOfUser}!`);
+    }
   } else {
     console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".\nLet's try again, ${nameOfUser}!`);
   }
