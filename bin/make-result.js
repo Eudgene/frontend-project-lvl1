025595e7046ff2a97c9508a6053f5493af088d4c
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-31 07:26:32 +0300

    fix make-result and brain-even

--- bin/make-result.js
+++ bin/make-result.js
@@ -10,7 +10,7 @@ function makeResult() {
     console.log('Correct!');
   } else {
     console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".\nLet's try again, !`);
-    break;
+    return;
   }
 }
 
