80b857c3d9b33ba2c74e7fe38a5a1fd41d48abcf
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-03-31 07:40:48 +0300

    fix make-result and brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -11,7 +11,7 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 for (let counter = 0; counter <= 3; counter += 1) {
   if (counter < 3) {
-    makeResult();
+    console.log(makeResult());
   } else if (counter === 3) {
     console.log(`Congratulations, ${nameOfUser}!`);
   }
