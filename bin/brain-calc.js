7a6fbb569c62f36f4e5e27fc72118ae2d7fe50b5
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-01 08:02:25 +0300

    add brain-calc.js with logic

---
+++ bin/brain-calc.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import makeResult from './make-result.js';
+import getnameOfUser from './get-name.js';
+
+console.log('Welcome to the Brain Games!');
+const nameOfUser = getnameOfUser();
+console.log(`Hi, ${nameOfUser}!`);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let counter = 0; counter <= 3; counter += 1) {
+  if (counter < 3) {
+    if (makeResult() === false) {
+      console.log(`Let's try again, ${nameOfUser}!`);
+      break;
+    }
+  } else if (counter === 3) {
+    console.log(`Congratulations, ${nameOfUser}!`);
+  }
+}
