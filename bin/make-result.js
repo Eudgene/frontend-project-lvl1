2499e0505d5acb05e0f2c3a4ef5f8e9154b82b7a
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-01 08:37:38 +0300

    test

--- bin/make-result.js
+++ bin/make-result.js
@@ -19,6 +19,7 @@ export function makeCalc() {
   const mathexpression = getRandomElement();
   const ynanswer = resultOfExpression(randomnumber1, randomnumber2, mathexpression);
   const answer = readlineSync.question(`Question: ${randomnumber1} ${mathexpression} ${randomnumber2}\nYour answer: `);
+  console.log(ynanswer, answer);
   if (answer === ynanswer) {
     console.log('Correct!');
     return true;
