6b9c43d072d7f13eb823481f36419f53d1f7d66e
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-02 08:13:15 +0300

    chenge brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 import { makeCalc } from './make-result.js';
-import getnameOfUser from './get-name.js';
+import Engine from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
+const answer = 'What is the result of the expression?';
+Engine(answer, makeCalc);
+/*console.log('Welcome to the Brain Games!');
 const nameOfUser = getnameOfUser();
 console.log(`Hi, ${nameOfUser}!`);
 
@@ -18,4 +20,4 @@ for (let counter = 0; counter <= 3; counter += 1) {
   } else if (counter === 3) {
     console.log(`Congratulations, ${nameOfUser}!`);
   }
-}
+}*/
