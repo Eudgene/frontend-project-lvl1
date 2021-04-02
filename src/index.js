21465dec5cddaf6dd9a49e7b0b8a30ce129deee3
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-02 08:10:32 +0300

    fix index.js after linter

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ function Engine(answerFirst, functionToRezult) {
   console.log('Welcome to the Brain Games!');
   const nameOfUser = getnameOfUser();
   console.log(`Hi, ${nameOfUser}!`);
-    
+
   console.log(answerFirst);
-    
+
   for (let counter = 0; counter <= 3; counter += 1) {
     if (counter < 3) {
       if (functionToRezult() === false) {
