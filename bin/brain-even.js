931b1c2ff1453870bf940cf0214a244d1fed4f07
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-02 08:04:21 +0300

    change brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 
 import makeResult from './make-result.js';
-import getnameOfUser from './get-name.js';
+import Engine from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
+const answer = 'Answer "yes" if the number is even, otherwise answer "no".';
+Engine(answer, makeResult);
+/*console.log('Welcome to the Brain Games!');
 const nameOfUser = getnameOfUser();
 console.log(`Hi, ${nameOfUser}!`);
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log();
 
 for (let counter = 0; counter <= 3; counter += 1) {
   if (counter < 3) {
-    if (makeResult() === false) {
+    if ( === false) {
       console.log(`Let's try again, ${nameOfUser}!`);
       break;
     }
   } else if (counter === 3) {
     console.log(`Congratulations, ${nameOfUser}!`);
   }
-}
+}*/
