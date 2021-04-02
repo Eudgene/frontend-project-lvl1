4a74a5bead2adf0500dcf9e6a70f1c98fa3b61f6
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-02 08:03:50 +0300

    add logic to index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import getnameOfUser from './get-name.js';
+
+function Engine(answerFirst, functionToRezult) {
+  console.log('Welcome to the Brain Games!');
+  const nameOfUser = getnameOfUser();
+  console.log(`Hi, ${nameOfUser}!`);
+    
+  console.log(answerFirst);
+    
+  for (let counter = 0; counter <= 3; counter += 1) {
+    if (counter < 3) {
+      if (functionToRezult() === false) {
+        console.log(`Let's try again, ${nameOfUser}!`);
+        break;
+      }
+    } else if (counter === 3) {
+      console.log(`Congratulations, ${nameOfUser}!`);
+    }
+  }
+}
+
+export default Engine;
