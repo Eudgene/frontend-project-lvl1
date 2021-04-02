4df56133e60da38493a0c6c412a33ad3f67bf23d
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-02 08:18:53 +0300

    change directori for games

--- bin/brain-even.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import makeResult from './make-result.js';
-import Engine from '../src/index.js';
-
-const answer = 'Answer "yes" if the number is even, otherwise answer "no".';
-Engine(answer, makeResult);
