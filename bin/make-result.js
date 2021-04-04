dfd2a5054d3dda3a3eca4e761f671c0c8993538a
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-04 12:13:27 +0300

    add makeGcd

--- bin/make-result.js
+++ bin/make-result.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 import {
-  getRandomInt, isNumberIsEven, getRandomElement, resultOfExpression,
+  getRandomInt, isNumberIsEven, getRandomElement, resultOfExpression, makeGcdNumber
 } from './make-randomnumber.js';
 
 function makeResult() {
@@ -21,6 +21,19 @@ export function makeCalc() {
   const mathexpression = getRandomElement();
   const ynanswer = resultOfExpression(randomnumber1, randomnumber2, mathexpression);
   const answer = readlineSync.question(`Question: ${randomnumber1} ${mathexpression} ${randomnumber2}\nYour answer: `);
+  if (answer === String(ynanswer)) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".`);
+  return false;
+}
+
+export function makeGcd() {
+  const randomnumber1 = getRandomInt(100);
+  const randomnumber2 = getRandomInt(100);
+  const ynanswer = makeGcdNumber(randomnumber1, randomnumber2);
+  const answer = readlineSync.question(`Question: ${randomnumber1} ${randomnumber2}\nYour answer: `);
   if (answer === String(ynanswer)) {
     console.log('Correct!');
     return true;
