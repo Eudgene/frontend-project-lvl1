6e10924b176246c6b89ab229ede2349d33293f9b
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-04 12:12:44 +0300

    change answer and function

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import makeResult from '../bin/make-result.js';
+import { makeGcd } from '../bin/make-result.js';
 import Engine from '../src/index.js';
 
 const answer = 'Find the greatest common divisor of given numbers.';
-Engine(answer, makeResult);
+Engine(answer, makeGcd);
