95df5ea0d1f9b55d15edf74eea439ae2fcdd183a
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-06 08:14:26 +0300

    test fix5

--- games/make-result.js
+++ games/make-result.js
@@ -43,7 +43,7 @@ export function makeGcd() {
 }
 
 export function makeProgression() {
-  const arr = makeProgressionArray();
+  //const arr = makeProgressionArray();
   //const ynanswer = makeProgressionNumber(arr);
   const answer = readlineSync.question(`Question: ${makeProgressionArray()} `);
   if (answer === String(ynanswer)) {
