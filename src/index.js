3267e14b5dace39d66e4bff373e5ce4c6fb65567
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-07 09:03:50 +0300

    fix Engine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import getnameOfUser from '../games/get-name.js';
 
-/*function welcomAndAnswer(answerF, nameOf) {
-  console.log('Welcome to the Brain Games!');
-  console.log(`Hi, ${nameOf}!`);
-  console.log(answerF);
-}*/
-
 function Engine(answerFirst, functionToRezult) {
   console.log('Welcome to the Brain Games!');
   const nameOfUser = getnameOfUser();
   console.log(`Hi, ${nameOfUser}!`);
   console.log(answerFirst);
-  //welcomAndAnswer(answerFirst, nameOfUser);
 
   for (let counter = 0; counter <= 3; counter += 1) {
     if (counter < 3) {
