ad7560ceb2ab3ca0a962a0051356bd75e0f49b55
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-08 08:42:37 +0300

    fix brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { makePrime } from '../games/make-result.js';
+import { makePrime } from '../games/check-prime.js';
 import Engine from '../src/index.js';
 
 const answer = 'Answer "yes" if given number is prime. Otherwise answer "no".';
---
+++ games/check-prime.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+import { getRandomInt } from './make-randomnumber.js';
+
+function makePrimeNumber(num) {
+  const arr = [];
+  let result = 0;
+  for (let i = 1; i <= num; i += 1) {
+    if (num % i === 0) {
+      arr.push(i);
+    }
+  }
+  if (arr.length === 2) {
+    result = 'yes';
+  } else {
+    result = 'no';
+  }
+  return result;
+}
+
+export function makePrime() {
+  const randomnumber1 = getRandomInt(2, 100);
+  const ynanswer = makePrimeNumber(randomnumber1);
+  const answer = readlineSync.question(`Question: ${randomnumber1} \nYour answer: `);
+  if (answer === String(ynanswer)) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`"${answer}" is wrong answer ;(. Correct answer was "${ynanswer}".`);
+  return false;
+}
