64381c8910f2b96321eb6d61b11cada35b01242d
Eudgene8@mail.ru <eudgene@DESKTOP-8I1MLND.localdomain>
2021-04-08 08:22:42 +0300

    add check-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import makeResult from '../games/check-even';
+import makeResult from '../games/check-even.js';
 import Engine from '../src/index.js';
 
 const answer = 'Answer "yes" if the number is even, otherwise answer "no".';
